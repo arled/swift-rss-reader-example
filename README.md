da35b7b513012f7bc71fff7219e01aab8aaf14ae
arled <arledkola@gmail.com>
2018-10-23 12:50:11 +0100

    Minor refactoring and a new README GIF.

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 <strong>Great for kick starting your very own RSS reader app.</strong>
 
-Dear community, 
+Dear community,
 Feel free to contribute to this repo.
 
-![alt tag](https://media.giphy.com/media/dYiVe4w7ostLjZy3Sa/giphy.gif)
+![alt tag](https://media.giphy.com/media/fR4u5yG2qfjqGkZHnE/giphy.gif)
 
--- RSSwift/RSSwift/FeedListViewController.swift
+++ RSSwift/RSSwift/FeedListViewController.swift
@@ -9,97 +9,102 @@
 import UIKit
 
 class FeedListViewController: UITableViewController, XMLParserDelegate {
-    
+
     var myFeed : NSArray = []
     var feedImgs: [AnyObject] = []
     var url: URL!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 140
-        tableView.backgroundColor = UIColorFromRGB(rgbValue: 0x00B6ED)
+        tableView.backgroundColor = UIColorFromRGB(rgbValue: 0xdf4926)
         self.tableView.dataSource = self
         self.tableView.delegate = self
-        
+
         loadData()
     }
-    
+
     @IBAction func refreshFeed(_ sender: Any) {
 
         loadData()
     }
-    
+
     func loadData() {
         url = URL(string: "http://feeds.skynews.com/feeds/rss/technology.xml")!
         loadRss(url);
     }
-    
+
     func loadRss(_ data: URL) {
-        // XmlParserManager instance/object/variable
+
+        // XmlParserManager instance/object/variable.
         let myParser : XmlParserManager = XmlParserManager().initWithURL(data) as! XmlParserManager
-        // Put feed in array
+
+        // Put feed in array.
         feedImgs = myParser.img as [AnyObject]
         myFeed = myParser.feeds
         tableView.reloadData()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "openPage" {
             let indexPath: IndexPath = self.tableView.indexPathForSelectedRow!
             let selectedFURL: String = (myFeed[indexPath.row] as AnyObject).object(forKey: "link") as! String
-            // Instance of our feedpageviewcontrolelr
+
+            // Instance of our feedpageviewcontrolelr.
             let fivc: FeedItemViewController = segue.destination as! FeedItemViewController
             fivc.selectedFeedURL = selectedFURL as String
         }
     }
-    
-    // MARK: - Table view data source
+
+    // MARK: - Table view data source.
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myFeed.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.backgroundColor = UIColor.clear
         cell.detailTextLabel?.backgroundColor = UIColor.clear
-        
+
         if indexPath.row % 2 == 0 {
-            cell.backgroundColor = UIColor(white: 1, alpha: 0.1)
+            cell.backgroundColor = UIColor(white: 1, alpha: 0)
         } else {
-            cell.backgroundColor = UIColor(white: 1, alpha: 0.2)
+            cell.backgroundColor = UIColor(white: 1, alpha: 0.1)
         }
 
         // Load feed iamge.
         let url = NSURL(string:feedImgs[indexPath.row] as! String)
         let data = NSData(contentsOf:url! as URL)
         var image = UIImage(data:data! as Data)
+
         image = resizeImage(image: image!, toTheSize: CGSize(width: 70, height: 70))
+
         let cellImageLayer: CALayer?  = cell.imageView?.layer
+
         cellImageLayer!.cornerRadius = 35
         cellImageLayer!.masksToBounds = true
+
         cell.imageView?.image = image
         cell.textLabel?.text = (myFeed.object(at: indexPath.row) as AnyObject).object(forKey: "title") as? String
         cell.textLabel?.textColor = UIColor.white
         cell.textLabel?.numberOfLines = 0
         cell.textLabel?.lineBreakMode = .byWordWrapping
-        
         cell.detailTextLabel?.text = (myFeed.object(at: indexPath.row) as AnyObject).object(forKey: "pubDate") as? String
         cell.detailTextLabel?.textColor = UIColor.white
-        
+
         return cell
     }
-    
+
     func UIColorFromRGB(rgbValue: UInt) -> UIColor {
         return UIColor(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
@@ -108,17 +113,16 @@ class FeedListViewController: UITableViewController, XMLParserDelegate {
             alpha: CGFloat(1.0)
         )
     }
-    
+
     func resizeImage(image:UIImage, toTheSize size:CGSize)->UIImage{
-        
-        
+
         let scale = CGFloat(max(size.width/image.size.width,
                                 size.height/image.size.height))
         let width:CGFloat  = image.size.width * scale
         let height:CGFloat = image.size.height * scale;
-        
+
         let rr:CGRect = CGRect(x: 0, y: 0, width: width, height: height)
-        
+
         UIGraphicsBeginImageContextWithOptions(size, false, 0);
         image.draw(in: rr)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
